9798545b8eb8e05dfd501b8b447effe5dd50d9b4
Lionel D <lionel.deri@gmail.com>
2023-10-21 23:06:31 +0200

    solution 1

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+
+$contactEmail = 'contact@yavuz.fr';
+$twig->addGlobal('contactEmail', $contactEmail);
+
+
+?>
--- public/index.php
+++ public/index.php
@@ -6,4 +6,4 @@ require_once __DIR__ . '/../config/twig.php';
 
 $name = 'Wilder';
 
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('layout.html.twig', ['name' => $name]);
